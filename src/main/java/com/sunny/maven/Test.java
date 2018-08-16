d1182b6fc60a415dd84cb42f061ce29b15681c4b
zhaoyunxing92 <2385585770@qq.com>
2018-08-17 01:44:22 +0800

    添加：生成文件插件

---
+++ src/main/java/com/sunny/maven/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright(C) 2018 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package com.sunny.maven;
+
+/**
+ * @author zhaoyunxing
+ * @class: com.sunny.maven.Test
+ * @date: 2018-08-17 00:15
+ * @des:
+ */
+public class Test {
+}
